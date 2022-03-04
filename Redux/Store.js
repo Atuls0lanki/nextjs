a48d719c0031ad90d7e9df64c749b86d52d8cde2
Atul <atuls0lanki>
2022-03-04 17:30:02 +0530

    chart js added

--- Redux/Store.js
+++ Redux/Store.js
@@ -1,15 +1,13 @@
 import { createStore } from "redux";
 import rootReducer from "./RootReducer";
 import Cookies from "js-cookie";
-import { parseCookies } from "../utility/parseCookies";
+import parseCookies from "../utility/parseCookies";
 
 export const LocalStoreName = "persistState";
 
 function saveToLocalStorage(state) {
   try {
     const serialState = JSON.stringify(state);
-    console.log(state), serialState;
-
     Cookies.set(LocalStoreName, serialState);
   } catch (e) {
     console.warn(e);
@@ -18,10 +16,10 @@ function saveToLocalStorage(state) {
 
 function loadFromLocalStorage() {
   try {
-    const serialState = parseCookies();
-    if (serialState === null) return undefined;
-    console.log(serialState.persistState);
-    return JSON.parse(serialState.persistState);
+    const Data = Cookies.get(LocalStoreName);
+    if (Data === undefined || typeof window === "undefined") return undefined;
+    console.log(Data);
+    return JSON.parse(Data);
   } catch (e) {
     console.warn(e);
     return undefined;
